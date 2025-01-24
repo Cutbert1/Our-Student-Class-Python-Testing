93681e6262d50695a277aa3df478f0abcc5b9bf8
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2025-01-24 15:55:06 +0000

    Add mocking to test successful and unsuccessful requests  for course_schedule method

--- test_student.py
+++ test_student.py
@@ -1,6 +1,7 @@
 import unittest
 from student import Student
 from datetime import timedelta
+from unittest.mock import patch
 
 class TestStudent(unittest.TestCase):
 
@@ -51,8 +52,26 @@ class TestStudent(unittest.TestCase):
         self.student.apply_extension(5)
 
         self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
-        
 
+    # test function test_course_schedule_success, to test successful request
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    # test function test_course_schedule_failed, to test failed request
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            mocked_get.return_value.text = "Something went wrong with the request!"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
+
+    
 if __name__ == "__main__":
     unittest.main()
 
