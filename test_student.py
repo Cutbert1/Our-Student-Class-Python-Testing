2ae25e33cdf6e9944b255c0c1fba13b834206e5e
Cutbert Ogbozor <cutbertogbozor@yahoo.co.uk>
2025-01-24 11:05:05 +0000

    Created a test called test_alert_santa to  alert_santa that we still need to create in the Student class

--- test_student.py
+++ test_student.py
@@ -3,11 +3,22 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
+    # test for Student class to instantiated it and 
+    # call the full_name method on it to assert 
+    # whether it returned the first_name and last_name property values separated by a space
     def test_full_name(self):
         student = Student("John", "Doe")
-
         self.assertEqual(student.full_name, "John Doe")
+    
+    # test for a function called alert_santa to change the naughty_list property to True.
+    def test_alert_santa(self):
+        student = Student("John", "Doe")
+        student.alert_santa()
+        
+        self.assertTrue(student.naughty_list)
 
 
 if __name__ == "__main__":
     unittest.main()
+
+  
